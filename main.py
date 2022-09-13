1d86e51cfece5a8f60035dee052d4b992b0cc2d0
Alexandr Vorobjev <vorobjev.alexandr-2017@yandex.ru>
2022-09-13 14:50:27 +0300

    Сумма элементов стояящих на нечетной позиции

--- main.py
+++ main.py
@@ -7,9 +7,8 @@
 
 def sum_elem_index(*args):
     sum = 0
-    for i in range(0, len(list)):
-        if i % 2 != 0:
-            sum = list[i] + sum
+    for i in range(1, len(list), 2):
+        sum += list[i]
     return sum
 list = [2, 3, 5, 9, 3]
 result = sum_elem_index(list)
